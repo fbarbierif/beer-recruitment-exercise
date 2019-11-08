87e2c4cbfaf26ef73f99fb7d91e57ea1d024a65d
Fabian Barbieri <f.barbieri.f@gmail.com>
2019-11-08 10:55:47 +0100

    Init Fresco and Realm

--- app/src/main/java/com/example/beerrecruitmentexercise/MainApplication.java
+++ app/src/main/java/com/example/beerrecruitmentexercise/MainApplication.java
@@ -2,10 +2,24 @@ package com.example.beerrecruitmentexercise;
 
 import android.app.Application;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class MainApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Fresco.initialize(this);
+
+        Realm.init(this);
+        final RealmConfiguration config = new RealmConfiguration
+                .Builder()
+                .deleteRealmIfMigrationNeeded()
+                .build();
+        Realm.setDefaultConfiguration(config);
     }
 }
